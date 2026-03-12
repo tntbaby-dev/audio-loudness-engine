61db84be9ff171289ed950ad8f7f75a05b73156a
TNT Baby <mac@TNT.local>
2026-03-12 11:09:42 +0100

    Checkpoint loudness intelligence engine v1

--- test_project/waveform.py
+++
@@ -1,9 +0,0 @@
-import soundfile as sf
-import matplotlib.pyplot as plt
-
-audio, sample_rate = sf.read("input.wav")
-print("Sample Rate:", sample_rate)
-
-plt.plot(audio)
-plt.title("Waveform")
-plt.show()
